d520472f4a0fa73e4ee01a5a4c658bf60b7cc0d1
lrichard <lawric49@gmail.com>
2025-06-26 21:07:26 +0200

    mise en place du cookie pour le theme en js

--- js/jour-nuit.js
+++ js/jour-nuit.js
@@ -4,6 +4,15 @@ const themeIcon = toggleButton.querySelector("img");
 let modeNuit = false;
 let angle = 0;
 
+function getCookie(name) {
+  const cookies = document.cookie.split('; ');
+  for (let cookie of cookies) {
+    const [key, value] = cookie.split('=');
+    if (key === name) return value;
+  }
+  return null;
+}
+
 toggleButton.addEventListener("click", () => {
   // Incrémentation de 180°
   angle += 180;
@@ -22,5 +31,21 @@ toggleButton.addEventListener("click", () => {
     // Thème clair/sombre
     document.body.classList.toggle("nuit", modeNuit);
     document.querySelector("header").classList.toggle("header-nuit", modeNuit);
+    document.cookie = `theme=${modeNuit}; path=/`;
   }, 175); // Moitié du temps d'animation
+  
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  const theme = getCookie("theme");
+
+  if (theme === "true") {
+    // Appliquer le thème nuit
+    document.body.classList.add("nuit");
+    document.querySelector("header").classList.add("header-nuit");
+    themeIcon.src = "element/image/moon.png";
+    modeNuit = true;
+    angle = 180;
+    themeIcon.style.transform = `rotate(${angle}deg)`;
+  }
 });
